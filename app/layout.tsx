d17b16027c58fb568e19a5be4f748d6cb1f34750
kanghyki <kanghyki@gmail.com>
2023-10-05 05:08:29 +0900

    design: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={roboto.className}>
         <Nav />
         {children}
