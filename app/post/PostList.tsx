b66d15da0f5f9c76442ea2049b158e98f893d41b
kanghyki <kanghyki@gmail.com>
2023-10-14 21:01:59 +0900

    refactor: remove PostAcList & fix: debounce

--- app/post/PostList.tsx
+++ app/post/PostList.tsx
@@ -1,10 +1,10 @@
 'use client';
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { IBlogPost } from '@/src/BlogPost';
+import { Searcher } from '@/src/searcher/Searcher';
 import styles from './PostList.module.css';
 import PostListItem from './PostListItem';
 import CategoryButton from '../component/CategoryButton';
-import { IBlogPost } from '@/src/BlogPost';
-import { Searcher } from '@/src/searcher/Searcher';
 
 type PropsPostList = {
   posts: IBlogPost[];
@@ -15,22 +15,12 @@ type PropsPostList = {
 export default function PostList(props: PropsPostList) {
   const [category, setCategory] = useState<string | undefined>(undefined);
   const [searchText, setSearchText] = useState<string | undefined>(undefined);
-  const [acList, setAcList] = useState<string[]>([]);
-  const [filteredPosts, setFilteredPosts] = useState<IBlogPost[]>(props.posts);
+  const [posts, setPosts] = useState<IBlogPost[]>(props.posts);
   const searcher = useMemo(() => {
     const s = new Searcher({ caseInsensitive: true });
     s.getIndexer().fromJson(props.indexJson);
     return s;
   }, [props]);
-
-  const debounce = useCallback((fn: () => void, ms: number = 250): (() => void) => {
-    let timeout: NodeJS.Timeout;
-
-    return () => {
-      clearTimeout(timeout);
-      timeout = setTimeout(fn, ms);
-    };
-  }, []);
 
   const update = useCallback((): void => {
     let categoryPosts: IBlogPost[] = [];
@@ -39,59 +29,34 @@ export default function PostList(props: PropsPostList) {
     else categoryPosts = props.posts;
 
     if (searchText) {
-      setAcList(searcher.autoComplete(searchText, { max: 5 }));
       const searchPostIds = searcher.search(searchText);
-      setFilteredPosts(categoryPosts.filter(cp => searchPostIds.includes(cp.id)));
+      setPosts(categoryPosts.filter(cp => searchPostIds.includes(cp.id)));
     } else {
-      setAcList([]);
-      setFilteredPosts(categoryPosts);
+      setPosts(categoryPosts);
     }
   }, [searchText, category, props, searcher]);
 
   useEffect(() => {
-    debounce(update)();
-  }, [searchText, category, debounce, update]);
+    const timeout = setTimeout(() => {
+      update();
+    }, 250);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [searchText, category, update]);
 
   return (
     <>
       <CategoryList categories={props.categories} setCategory={setCategory} select={category} />
       <div className={styles.status_bar}>
-        <PostListInfo count={filteredPosts.length} category={category} searchString={searchText} />
+        <PostListInfo count={posts.length} category={category} searchString={searchText} />
         <SearchInput setSearchText={setSearchText} />
       </div>
-      <PostAcList acList={acList} setSearchText={setSearchText} />
       <ul>
-        {filteredPosts.map((e: IBlogPost) => (
+        {posts.map((e: IBlogPost) => (
           <PostListItem post={e} key={e.id} />
         ))}
       </ul>
-    </>
-  );
-}
-
-type PropsAcList = {
-  acList: string[];
-  setSearchText: (s: string) => void;
-};
-
-function PostAcList(props: PropsAcList) {
-  return (
-    <>
-      {props.acList.length > 0 && (
-        <p className={styles.auto_complete}>
-          {`일치하는 검색어*`}
-          {props.acList.map(e => (
-            <button
-              key={e}
-              onClick={() => {
-                props.setSearchText(e);
-              }}
-            >
-              {e}
-            </button>
-          ))}
-        </p>
-      )}
     </>
   );
 }
